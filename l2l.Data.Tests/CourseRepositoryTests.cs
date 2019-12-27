f324201a462be584364eb9426e488d30525465a3
lmeidl <lmeidl@host-ltd.hu>
2019-12-27 03:12:57 +0100

    #4, #5, #6: repo test w/ xUnit                  + CourseRepo skeleton

---
+++ l2l.Data.Tests/CourseRepositoryTests.cs
@@ -0,0 +1,34 @@
+using System;
+using Xunit;
+using l2l.Data.Model;
+using l2l.Data.Repository;
+using FluentAssertions;
+
+namespace l2l.Data.Tests
+{
+    /// <summary>
+    /// CRUD and list tests
+    /// </summary>
+    public class CourseRepositoryTests
+    {
+        [Fact]
+        public void CourseRepositoryTests_AddedCoursesShouldAppearInRepository()
+        {
+            //Arrange
+            var sut = new CourseRepository();
+            var course = new Course { Id = 1, Name = "Test Course"};
+            
+            //Act
+            sut.Add(course);
+            var result = sut.GetById(course.Id);
+            
+            //Assert
+            Assert.NotNull(result);
+
+            //antipattern: az Equals override-olása
+            //Assert.Equal(course, result);
+            result.Should().BeEquivalentTo(course);
+
+        }
+    }
+}
---
+++ l2l.Data/Repository/CourseRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using l2l.Data.Model;
+
+namespace l2l.Data.Repository
+{
+    public class CourseRepository
+    {
+        public void Add(Course course)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Course GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
